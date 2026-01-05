c48f4ee52b5fdbe1a80972bf5fcac39e15c9df3b
adli daffa <adli.daffa5@gmail.com>
2026-01-05 21:24:25 +0700

    variable and mutability

---
+++ 3_1_variables_and_mutability/src/main.rs
@@ -0,0 +1,25 @@
+const THREE_HOURS_IN_SECONDS: u64 = 60 * 60 * 3;
+
+fn main() {
+    let mut x = 5;
+    println!("The value of x is {x}");
+    x = 6;
+    println!("The value of x is {x}");
+
+    println!("Constant value {THREE_HOURS_IN_SECONDS}");
+
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is: {x}");
+
+    let spaces = "   ";
+    let spaces = spaces.len();
+    println!("spaces is: {spaces}");
+}
