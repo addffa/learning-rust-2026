bb5d05db2f8ee469da59ac77f855d7098252cb2b
adli daffa <adli.daffa5@gmail.com>
2026-01-08 21:47:40 +0700

    comments

---
+++ 3_4_comments/src/main.rs
@@ -0,0 +1,8 @@
+// this is simple command
+// So we're doing something complicated here, long enough that we need
+// multiple lines of comments to do it! Whew! Hopefully, this comment will
+// explain what's going on.
+fn main() {
+    // comment format before line of code usually used
+    println!("Hello, world!"); // but can also comment at the end of lines containing code
+}
