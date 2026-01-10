ce44b74f30decbbc81bf14fe4a1fe4cc20054415
adli daffa <adli.daffa5@gmail.com>
2026-01-10 21:27:43 +0700

    control flow: loop

--- 3_5_control_flow/src/main.rs
+++ 3_5_control_flow/src/main.rs
@@ -24,4 +24,50 @@ fn main() {
     let condition = true;
     let number = if condition { 5 } else { 6 };
     println!("the value of number is {number}");
+
+    // repetition with loop
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+        if counter == 2 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is: {result}");
+
+    // loop labels
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count: {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining: {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+        count += 1;
+    }
+    println!("end count: {count}");
+
+    // while loop
+    let  mut number = 3;
+    while number > 0 {
+        println!("number: {number}");
+        number -= 1;
+    }
+    println!("end while loop: {number}");
+
+    // for loop
+    let a = [1, 2, 3, 4, 5];
+    for element in a {
+        println!("element: {element}");
+    }
 }
