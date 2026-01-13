2db62f3914579c3efbe6f8505f12ed488fd0cf01
adli daffa <adli.daffa5@gmail.com>
2026-01-13 21:03:47 +0700

    references_and_borrowing: borrowing

--- 4_2_references_and_borrowing/src/main.rs
+++ 4_2_references_and_borrowing/src/main.rs
@@ -4,8 +4,35 @@ fn main() {
     let len = calculate_length(&s1);
 
     println!("len value: {len}");
+
+    let mut s = String::from("hello");
+    change(&mut s);
+    println!("s now: {s}");
+
+    let mut s = String::from("hello");
+
+    {
+        let _r1 = &mut s;
+    } // r1 goes out of scope here, so we can make a new reference with no problems.
+
+    let _r2 = &mut s;
+
+    let mut s = String::from("hello");
+
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    println!("{r1} and {r2}");
+    // Variables r1 and r2 will not be used after this point.
+
+    let r3 = &mut s; // no problem
+    println!("{r3}");
 }
 
 fn calculate_length(s: &String) -> usize {
     s.len()
 }
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
+
