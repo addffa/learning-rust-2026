b270be34aeb0797d2b1927e32be1c1a451450bc9
adli daffa <adli.daffa5@gmail.com>
2026-01-18 22:30:30 +0700

    tuple struct and unit struct

--- 5_1_defining_structs/src/main.rs
+++ 5_1_defining_structs/src/main.rs
@@ -5,6 +5,13 @@ struct User {
     sign_in_count: u64,
 }
 
+// Tuple Structs
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
+// Unit-Like Structs
+struct AlwaysEqual;
+
 fn main() {
     let mut user1 = User {
         active: true,
@@ -15,5 +22,31 @@ fn main() {
 
     user1.email = String::from("anotheremail@example.com");
 
-    println!("username: {}", user1.username);
+    println!("username: {}, email: {}, active: {}, sign_in: {}", user1.username, user1.email, user1.active, user1.sign_in_count);
+
+    let user = build_user(String::from("username"), String::from("user@mail.com"));
+    println!("build user: {}", user.username);
+
+    let user2 = User {
+        username: String::from("user2"),
+        ..user1
+    };
+    println!("username updated: {}", user2.username);
+
+    let color = Color(12, 16, 20);
+    println!("Color: ({}, {}, {})", color.0, color.1, color.2);
+
+    let point = Point(3, 4, 5);
+    println!("Point: ({}, {}, {})", point.0, point.1, point.2);
+
+    let subject = AlwaysEqual;
+}
+
+fn build_user(username: String, email: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1,
+    }
 }
