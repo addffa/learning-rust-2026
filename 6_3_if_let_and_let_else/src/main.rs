c8061c985faf7ef0fe136fe2853bade23ed97ed1
addffa <adli.daffa5@gmail.com>
2026-01-23 21:41:39 +0700

    if let and let else statemetn

---
+++ 6_3_if_let_and_let_else/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let config_max = Some(3u8);
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {max}");
+    } else {
+        println!("No config");
+    }
+
+    let Some(config) = config_max else {
+        println!("Config is None");
+        return;
+    };
+    println!("Config: {config}");
+}
